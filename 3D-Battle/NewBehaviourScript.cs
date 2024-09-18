6d34248fda5100786bb1dd1e52c050db3e8b34f9
fikrysetiaone <fikrysetiawan13@gmail.com>
2024-09-18 15:54:17 +0700

    ---

---
+++ 3D-Battle/NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    float moveSpeed = 7f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+}
